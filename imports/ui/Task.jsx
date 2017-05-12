efd8b1369e752067d66f111ff8195011dec5b928
FausticSun <fausticsun@gmail.com>
2017-05-12 16:37:50 +0800

    Update Task component to add features

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,8 +1,42 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+
+import { Tasks } from '../api/tasks.js';
 
 // Task component - represents a single task item
-export default function Task(props) {
-  return <li>{props.task.text}</li>;
+export default class Task extends Component {
+  toggleChecked() {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(this.props.task._id, {
+      $set: { checked: !this.props.task.checked },
+    });
+  }
+
+  deleteThisTask() {
+    Tasks.remove(this.props.task._id);
+  }
+
+  render() {
+    // Give tasks a different className when they are checked off,
+    // so that we can style them nicely in CSS
+    const taskClassName = this.props.task.checked ? 'checked' : '';
+
+    return (
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+          &times;
+        </button>
+
+        <input
+          type="checkbox"
+          readonly
+          checked={this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
+
+        <span className="text">{this.props.task.text}</span>
+      </li>
+    );
+  }
 }
 
 Task.propTypes = {
