e0355852451a067d624bd1e84e52b6e0b9d46984
FausticSun <fausticsun@gmail.com>
2017-05-12 14:39:21 +0800

    Almost enforced Airbnb style guide

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,33 +4,35 @@ import Task from './Task.jsx';
 
 // App component, represents the whole app
 export default class App extends Component {
-    getTasks() {
-        return [
-            { _id: 1, text: 'This is task 1' },
-            { _id: 2, text: 'This is task 2' },
-            { _id: 3, text: 'This is task 3' },
-        ];
-    }
+  constructor() {
+    super();
+    this.taskList = [
+      { _id: 1, text: 'This is task 1' },
+      { _id: 2, text: 'This is task 2' },
+      { _id: 3, text: 'This is task 3' },
+    ];
+  }
+  getTasks() {
+    return this.taskList;
+  }
 
-    renderTasks() {
-        return this.getTasks().map((task) => (
-           <Task key={task._id} task={task} />
-        ));
-    }
+  renderTasks() {
+    return this.getTasks().map(task => (
+      <Task key={task.id} task={task} />
+    ));
+  }
 
-    render() {
-        return (
-            <div className="container">
-                <header>
-                    <h1>Todo List</h1>
-                </header>
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+        </header>
 
-                <ul>
-                    {this.renderTasks()}
-                </ul>
-            </div>
-        );
-    }
+        <ul>
+          {this.renderTasks()}
+        </ul>
+      </div>
+    );
+  }
 }
-
-
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,16 +1,12 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 
 // Task component - represents a single task item
-export default class Task extends Component {
-    render() {
-        return (
-            <li>{this.props.task.text}</li>
-        );
-    }
+export default function Task(props) {
+  return <li>{props.task.text}</li>;
 }
 
 Task.propTypes = {
-    // This Component gets the task to display through a React prop.
-    // We can use propTypes to indicate it is required
-    task: PropTypes.object.isRequired,
+  // This Component gets the task to display through a React prop.
+  // We can use propTypes to indicate it is required
+  task: PropTypes.object.isRequired,
 };
