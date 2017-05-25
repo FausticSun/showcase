9e9072c4d243be3e1a19a13cf1f255590cc7e2bc
FausticSun <fausticsun@gmail.com>
2017-05-25 12:15:08 +0800

    Include sign in form

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -5,6 +5,7 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Tasks } from '../api/tasks.js';
 
 import Task from './Task.jsx';
+import AccountsUIWrapper from './AccountsUIWrapper';
 
 // App component, represents the whole app
 class App extends Component {
@@ -61,6 +62,8 @@ class App extends Component {
             />
             Hide Completed Tasks
           </label>
+
+          <AccountsUIWrapper />
 
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
             <input
