bd5dac240bde94327a07b646edb2163c521af926
FausticSun <fausticsun@gmail.com>
2017-05-25 16:54:29 +0800

    Prepare the database for each test

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -1,10 +1,26 @@
 /* eslint-env mocha */
 
 import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+
+import { Tasks } from './tasks.js';
 
 if (Meteor.isServer) {
   describe('Tasks', () => {
     describe('methods', () => {
+      const userid = random.id();
+      let taskId;
+
+      beforeEach(() => {
+        Tasks.remove({});
+        taskId = Tasks.insert({
+          text: 'test task',
+          createAt: new Date(),
+          owner: userId,
+          username: 'tmeasday',
+        });
+      });
+
       it('can delete owned task', () => {
       });
     });
