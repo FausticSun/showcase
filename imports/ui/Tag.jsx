7845e15bf395d067b3ebec41864f3c17e6ef3a5c
WEIHAODESKTOP\WeiHao <tanweihao94@gmail.com>
2017-06-22 11:41:16 +0800

    Able to tag now. Overlay tagholder div over the image so that you directly click, can work on saving later

---
+++ imports/ui/Tag.jsx
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+
+// Task component - represents a single todo item
+export default class Tag extends Component {
+  constructor(props) {
+    super(props);
+    this.divStyle = {
+      left: this.props.number[0],
+      top:  this.props.number[1]
+    };
+  }
+  render () {
+      return (
+          <div className="tag" style={this.divStyle}>{this.props.number[0]} + {this.props.number[1]}</div>
+      );
+  }
+}
