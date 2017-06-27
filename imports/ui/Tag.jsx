39593410d48b2458c1a0908d882edd7097e31fb0
WEIHAODESKTOP\WeiHao <tanweihao94@gmail.com>
2017-06-27 13:35:43 +0800

    Renamed some variables in Tag.jsx,  and added comments

--- imports/ui/Tag.jsx
+++ imports/ui/Tag.jsx
@@ -5,18 +5,19 @@ import ReactDOM from 'react-dom';
 export default class Tag extends Component {
   constructor(props) {
     super(props);
+    //Array is [ left %, top %, ChronologicalTagNumber, LabelDescription, LabelURL]
     this.divStyle = {
-      left: this.props.number[0],
-      top:  this.props.number[1]
+      left: this.props.objArray[0],
+      top:  this.props.objArray[1]
     };
-    this.labelName = 'label' + this.props.number[2];
+    this.labelName = 'label' + this.props.objArray[2];
   }
 
   render () {
       return (
         <div className='tag' style={this.divStyle} >
-          <div className="singleTag">{this.props.number[2]}</div>
-          <div className="label"><a className='labelUrl' href={this.props.number[4]}>{this.props.number[3]}</a></div>
+          <div className="singleTag">{this.props.objArray[2]}</div>
+          <div className="label"><a className='labelUrl' href={this.props.objArray[4]}>{this.props.objArray[3]}</a></div>
         </div>
       );
   }
