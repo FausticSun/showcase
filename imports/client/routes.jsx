12c6512dbd3fbd04a4d57b34116e098ac1ed82c0
FausticSun <fausticsun@gmail.com>
2017-07-05 09:36:07 +0800

    Lint routes.jsx

--- imports/client/routes.jsx
+++ imports/client/routes.jsx
@@ -1,42 +1,39 @@
 import React from 'react';
 import { mount } from 'react-mounter';
 import { FlowRouter } from 'meteor/kadira:flow-router';
-import {MainLayout} from './layouts/MainLayout.jsx';
+import { MainLayout } from './layouts/MainLayout.jsx';
 import App from '../ui/App.jsx';
 import Upload from '../ui/Upload.jsx';
-import Post from '../ui/Post.jsx';
 import PostContainer from '../ui/PostContainer.jsx';
-import Hub from '../ui/Hub.jsx';
 import HubContainer from '../ui/HubContainer.jsx';
+
 FlowRouter.route('/', {
   action() {
     mount(MainLayout, {
-        content: (<App />)
-    })
-  }
-})
+      content: (<App />),
+    });
+  },
+});
 FlowRouter.route('/upload', {
   action() {
     mount(MainLayout, {
-        content: (<Upload />)
-    })
-  }
-})
+      content: (<Upload />),
+    });
+  },
+});
 
 FlowRouter.route('/p/:postId', {
-  action: function(params) {
-    console.log("Route(ID): " +params.postId)
+  action(params) {
     mount(MainLayout, {
-        content: (<PostContainer postId={params.postId}/>)
-    })
-  }
-})
+      content: (<PostContainer postId={params.postId} />),
+    });
+  },
+});
 
 FlowRouter.route('/hub/:hubName', {
-  action: function(params) {
-    console.log("Route(hub): " + params.hubName)
+  action(params) {
     mount(MainLayout, {
-        content: (<HubContainer hubName={params.hubName}/>)
-    })
-  }
-})
+      content: (<HubContainer hubName={params.hubName} />),
+    });
+  },
+});
