e96676b2b190d5735c9632c53534432fab522edb
FausticSun <fausticsun@gmail.com>
2017-07-08 11:36:15 +0800

    Lint Post.jsx

--- imports/ui/Post.jsx
+++ imports/ui/Post.jsx
@@ -1,16 +1,7 @@
 import React, { Component, PropTypes } from 'react';
-import { createContainer } from 'meteor/react-meteor-data';
-
-import { Canvases } from '../api/canvases.js';
-
 import Canvas from './Canvas.jsx';
-import AccountsUIWrapper from './AccountsUIWrapper.jsx';
 // App component - represents the whole app
 export default class Post extends Component {
-  constructor(props) {
-    super(props);
-    // console.log('FUCK THIS: ' + this.props.postId);
-  }
   renderCanvas() {
     return this.props.canvases.map(canvas => (
       <Canvas key={canvas._id} canvas={canvas} />
@@ -27,3 +18,7 @@ export default class Post extends Component {
     );
   }
 }
+
+Post.propTypes = {
+  canvases: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
