3d521f3ee0c11f401c5bae743485c5d350023bf2
FausticSun <fausticsun@gmail.com>
2017-07-08 12:54:08 +0800

    Lint PostContainer.jsx

--- imports/ui/PostContainer.jsx
+++ imports/ui/PostContainer.jsx
@@ -1,9 +1,9 @@
-import { Meteor } from 'meteor/meteor';
-import React, { Component, PropTypes } from 'react';
+/* global PostContainer:true */
+/* exported PostContainer */
+
 import { createContainer } from 'meteor/react-meteor-data';
 import { Canvases } from '../api/canvases.js';
 import Post from './Post.jsx';
-import Canvas from './Canvas.jsx';
 
 export default PostContainer = createContainer(({ postId }) => {
   const canvases = Canvases.find({ _id: postId }).fetch();
