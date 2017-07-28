40ad897ebe1fd91236970c3ee2d146e84d755f70
WEIHAODESKTOP\WeiHao <tanweihao94@gmail.com>
2017-07-28 15:29:16 +0800

    Can register to database!

--- imports/client/components/LoginAndRegister/Register.jsx
+++ imports/client/components/LoginAndRegister/Register.jsx
@@ -3,7 +3,7 @@ import { PropTypes } from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 import { FlowRouter } from 'meteor/kadira:flow-router';
-import { Button } from 'semantic-ui-react';
+import { Accounts } from 'meteor/accounts-base';
 import { Form } from 'semantic-ui-react';
 
 const RegisterStyle = {
@@ -26,13 +26,29 @@ class Register extends Component {
   onChangeHandler = (e, { name, value }) => {
     this.setState({ [name]: value });
   }
+  registerUser = () => {
+    const newUser = {
+      profile : {
+        name: this.state.name,
+      },
+      username  : this.state.userName,
+      password : this.state.password,
+    };
 
+    console.log(newUser);
+    Accounts.createUser({
+      name: this.state.name,
+      username: this.state.userName,
+      password: this.state.password,
+    }, (e) => {console.log(this.props.currentUser);});
+    FlowRouter.go("/");
+  }
   render() {
     const { name, password, userName, repeatPassword } = this.state;
 
     return (
       <div style={RegisterStyle}>
-        <Form size="big" onSubmit={this.onSubmitHandler}>
+        <Form size="big" onSubmit={this.registerUser}>
           <Form.Input name="name" value={name}  label='Name' placeholder="Name" onChange={this.onChangeHandler} />
           <Form.Input name="userName" value={userName} label='Desired Username'  placeholder="Username" onChange={this.onChangeHandler} />
           <Form.Input name="password" value={password} label='Password' type='password' placeholder="Password" onChange={this.onChangeHandler} />
