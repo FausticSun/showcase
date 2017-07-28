b7997e4f1a522f90f40becbab111944680fd4f78
WEIHAODESKTOP\WeiHao <tanweihao94@gmail.com>
2017-07-28 16:51:49 +0800

    Can register, auto login. Can login and fixed the header of siteheader

--- imports/client/components/LoginAndRegister/Login.jsx
+++ imports/client/components/LoginAndRegister/Login.jsx
@@ -3,7 +3,6 @@ import { PropTypes } from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 import { FlowRouter } from 'meteor/kadira:flow-router';
-import { Button } from 'semantic-ui-react';
 import { Form } from 'semantic-ui-react';
 
 const LoginStyle = {
@@ -17,21 +16,24 @@ class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
       userName: '',
-      repeatPassword: '',
+      password: '',
     };
   }
   onChangeHandler = (e, { name, value }) => {
     this.setState({ [name]: value });
   }
+  loginUser = () => {
 
+    Meteor.loginWithPassword(this.state.userName, this.state.password );
+    FlowRouter.go("/");
+  }
   render() {
     const { password, userName } = this.state;
 
     return (
       <div style={LoginStyle}>
-        <Form size="big" onSubmit={this.onSubmitHandler}>
+        <Form size="big" onSubmit={this.loginUser}>
           <Form.Input name="userName" value={userName} label='Username'  placeholder="Username" onChange={this.onChangeHandler} />
           <Form.Input name="password" value={password} label='Password' type='password' placeholder="Password" onChange={this.onChangeHandler} />
           <Form.Button>Log In</Form.Button>
--- imports/client/components/header/SiteHeader.jsx
+++ imports/client/components/header/SiteHeader.jsx
@@ -1,17 +1,39 @@
-import React from 'react';
-import { Menu } from 'semantic-ui-react';
-import Blaze from 'meteor/gadicc:blaze-react-component';
+import React, { Component } from 'react';
+import { Menu, Dropdown } from 'semantic-ui-react';
+import { Meteor } from 'meteor/meteor';
 import HubDropdown from './HubDropdown.jsx';
+import { createContainer } from 'meteor/react-meteor-data';
 
-const SiteHeader = () => (
-  <Menu>
-    <Menu.Item header className="showcase-logo" href="/"> Showcase </Menu.Item>
-    <HubDropdown />
-    <Menu.Menu position="right">
-      <Menu.Item name="upload" href="/upload" />
-      <Menu.Item name="login" href="/login" />
-    </Menu.Menu>
-  </Menu>
-);
+class SiteHeader extends Component {
+    logout = () => {
+      Meteor.logout();
+      FlowRouter.go('/');
+    }
+    render() {
+      if( this.props.currentUser ) {
+        $menuView = ( <Dropdown item text={this.props.currentUser.username} >
+                        <Dropdown.Menu>
+                          <Dropdown.Item>My Profile</Dropdown.Item>
+                          <Dropdown.Item onClick={this.logout}>Logout</Dropdown.Item>
+                        </Dropdown.Menu>
+                      </Dropdown>
+        );
+      } else {
+        $menuView = (<Menu.Item name="login" href="/login"/>);
+      }
+      return (
+        <Menu>
+          <Menu.Item header className="showcase-logo" href="/"> Showcase </Menu.Item>
+          <HubDropdown />
+          <Menu.Menu position="right">
+            <Menu.Item name="upload" href="/upload" />
+            {$menuView}
+           </Menu.Menu>
+        </Menu>
+        );
+    }
+}
 
-export default SiteHeader;
+export default createContainer(() => ({
+    currentUser: Meteor.user(),
+}), SiteHeader);
