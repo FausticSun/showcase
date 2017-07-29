f21041f3c1a3c6039332f0c481a715b310ea82f9
WEIHAODESKTOP\WeiHao <tanweihao94@gmail.com>
2017-07-29 16:04:20 +0800

    Can change profile picture. Fixed Eslint errors as well

--- imports/client/Settings.jsx
+++ imports/client/Settings.jsx
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import React, { Component, PropTypes } from 'react';
 import { Header, Icon, Form, Image, Button } from 'semantic-ui-react';
+import { FlowRouter } from 'meteor/kadira:flow-router';
 
 const settingsStyle = {
   display: 'flex',
@@ -12,15 +13,11 @@ const profilePicDisplayStyle = {
   margin: 'auto',
   width: '200px',
   height: '200px',
-}
+};
 // Settings component - represents a single canvas
 export default class Settings extends Component {
   constructor(props) {
     super(props);
-    console.log(`Person: ${Meteor.userId()}`);
-    const user = Meteor.users.findOne(Meteor.userId());
-    console.log(this.props.currentUser);
-    console.log(user);
     this.state = {
       editName: false,
       name: '',
@@ -37,66 +34,72 @@ export default class Settings extends Component {
     this.setState({ [name]: value });
   };
   imageUploadHandler = (e) => {
-    this.setState({
-      editPic: true,
-      tempProfilePic: window.URL.createObjectURL(e.target.files[0]),
-    });
+    const reader = new FileReader();
+    const file = e.target.files[0];
+    reader.onloadend = () => {
+      this.setState({
+        editPic: true,
+        tempProfilePic: reader.result,
+      });
+    };
+    reader.readAsDataURL(file);
   };
   updateUserSettings = () => {
-    if (this.state.name){
+    if (this.state.name) {
       Meteor.users.update(Meteor.userId(), { $set: { 'profile.name': this.state.name } });
     }
     if (this.state.password) {
       Accounts.changePassword(this.state.oldPassword, this.state.password);
     }
-
+    if (this.state.tempProfilePic) {
+      Meteor.users.update(Meteor.userId(), { $set: { 'profile.profilePic': this.state.tempProfilePic } });
+    }
     FlowRouter.go('/');
-  };
-  updateState = () => {
-    console.log(Meteor.user());
   };
   render() {
     const { currentUser } = this.props;
-    const { name, password, userName, repeatPassword, tempProfilePic } = this.state;
+    const { name, password, userName, repeatPassword } = this.state;
 
     let $profilePic = '';
     if (!currentUser) {
-      $profilePic = 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif'
-    } else if(!this.state.editPic){
+      $profilePic = 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif';
+    } else if (!this.state.editPic) {
       $profilePic = this.props.currentUser.profile.profilePic;
-    } else{
+    } else {
       $profilePic = this.state.tempProfilePic;
     }
 
     return (
       <article style={settingsStyle} className="postWrapper">
         { currentUser ?
-        <div>
-          <Header as='h2' icon >
-            <Icon name='settings' />
-            Account Settings
-            <Header.Subheader>
-              Hi {this.props.currentUser.profile.name}, Manage your preferences
-            </Header.Subheader>
-          </Header>
-          <Image src={$profilePic} shape="circular" style={profilePicDisplayStyle} />
-          <input
-            type="file"
-            ref={r => (this.file = r)}
-            style={{ display: 'none' }}
-            accept="image/*"
-            onChange={this.imageUploadHandler}
-          />
-          <Button primary onClick={() => this.file.click()}>Choose another profile picture</Button>
-          <Form size="large" onSubmit={this.updateUserSettings} >
-            <Form.Input name="name" value={name} label="Edit Name" placeholder={currentUser.profile.name} onChange={this.onChangeHandler} />
-            <Form.Input name="userName" value={userName} label="Edit Username" placeholder={currentUser.username} onChange={this.onChangeHandler} />
-            <Form.Input name="oldPassword" value={password} label="Old Password" type="password" placeholder="Old Password" onChange={this.onChangeHandler} />
-            <Form.Input name="password" value={password} label="Password" type="password" placeholder="Password" onChange={this.onChangeHandler} />
-            <Form.Input name="repeatPassword" value={repeatPassword} label="Repeat Password" type="password" placeholder="Repeat Password" onChange={this.onChangeHandler} />
-            <Form.Button>Update Settings</Form.Button>
-          </Form>
-        </div>
+          <div>
+            <Header as="h2" icon >
+              <Icon name="settings" />
+              Account Settings
+              <Header.Subheader>
+                Hi {this.props.currentUser.profile.name}, Manage your preferences
+              </Header.Subheader>
+            </Header>
+            <Image src={$profilePic} shape="circular" style={profilePicDisplayStyle} />
+            <input
+              type="file"
+              ref={r => (this.file = r)}
+              style={{ display: 'none' }}
+              accept="image/*"
+              onChange={this.imageUploadHandler}
+            />
+            <Button primary onClick={() => this.file.click()}>
+              Choose another profile picture
+            </Button>
+            <Form size="large" onSubmit={this.updateUserSettings} >
+              <Form.Input name="name" value={name} label="Edit Name" placeholder={currentUser.profile.name} onChange={this.onChangeHandler} />
+              <Form.Input name="userName" value={userName} label="Edit Username" placeholder={currentUser.username} onChange={this.onChangeHandler} />
+              <Form.Input name="oldPassword" value={password} label="Old Password" type="password" placeholder="Old Password" onChange={this.onChangeHandler} />
+              <Form.Input name="password" value={password} label="Password" type="password" placeholder="Password" onChange={this.onChangeHandler} />
+              <Form.Input name="repeatPassword" value={repeatPassword} label="Repeat Password" type="password" placeholder="Repeat Password" onChange={this.onChangeHandler} />
+              <Form.Button>Update Settings</Form.Button>
+            </Form>
+          </div>
         : <div>LOADING GIF</div> }
       </article>
     );
@@ -106,6 +109,10 @@ export default class Settings extends Component {
 Settings.propTypes = {
   currentUser: PropTypes.shape({
     username: PropTypes.string,
+    profile: PropTypes.shape({
+      name: PropTypes.string,
+      profilePic: PropTypes.string,
+    }),
   }),
 };
 
