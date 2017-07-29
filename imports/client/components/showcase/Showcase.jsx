c6667d432be05a73fb41b055d26ae7e67c2c81e7
FausticSun <FausticSun@gmail.com>
2017-07-29 22:04:55 +0800

    Added more showcase details

--- imports/client/components/showcase/Showcase.jsx
+++ imports/client/components/showcase/Showcase.jsx
@@ -16,15 +16,31 @@ class Showcase extends Component {
   }
 
   render() {
+    const showcaseData = this.props.showcaseData;
     return (
       <div style={showcaseStyle}>
         <Card fluid>
+          <Card.Content>
+            <Card.Header>
+              {showcaseData.title}
+            </Card.Header>
+          </Card.Content>
           <div style={{ position: 'relative' }}>
             <div onClick={this.clickHandler} >
-              <Image fluid src={this.props.showcaseData.imageSrc} />
-              <TagHolder tags={this.props.showcaseData.tags} />
+              <Image fluid src={showcaseData.imageSrc} />
+              <TagHolder tags={showcaseData.tags} />
             </div>
           </div>
+          <Card.Content>
+            <Card.Meta>
+              by {showcaseData.userName}{' '}
+              to {showcaseData.hubName}{' '}
+              at {showcaseData.createdAt.toString()}
+            </Card.Meta>
+            <Card.Description>
+              {showcaseData.description}
+            </Card.Description>
+          </Card.Content>
         </Card>
       </div>
     );
@@ -38,10 +54,13 @@ Showcase.propTypes = {
       name: PropTypes.string,
       url: PropTypes.string,
     })),
+    title: PropTypes.string,
+    description: PropTypes.string,
     hubName: PropTypes.string,
     imageSrc: PropTypes.string,
     likes: PropTypes.arrayOf(PropTypes.string),
     userId: PropTypes.string,
+    userName: PropTypes.string,
   }).isRequired,
 };
 
