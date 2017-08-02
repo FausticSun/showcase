7a8213e053c39e3ae6e842e1c2e35b45ad649cb4
FausticSun <fausticsun@gmail.com>
2017-08-02 23:55:15 +0800

    Refactored Likes.jsx

--- imports/client/components/showcase/Likes.jsx
+++ imports/client/components/showcase/Likes.jsx
@@ -1,43 +1,41 @@
-import React, { Component, PropTypes } from 'react';
+import React from 'react';
+import { PropTypes } from 'prop-types';
 import { Button, Popup } from 'semantic-ui-react';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 
-export default class Likes extends Component {
-  render () {
-    return (
+const Likes = props => (
+  <div>
+    {Meteor.user() ?
+      <Button
+        color="red"
+        content="Like"
+        icon="heart"
+        label={{ basic: true, color: 'red', pointing: 'left', content: `${props.numLikes}` }}
+        onClick={props.clickLike}
+      />
+        :
       <div>
-        { Meteor.user() ?
-          <Button
-            color="red"
-            content="Like"
-            icon="heart"
-            label={{ basic: true, color: 'red', pointing: 'left', content: `${this.props.numLikes}` }}
-            onClick={this.props.clickLike}
-          />
-          :
-          <div>
-            <Popup
-              trigger={
-                <Button
-                  color="red"
-                  content="Like"
-                  icon="heart"
-                  label={{ basic: true, color: 'red', pointing: 'left', content: `${this.props.numLikes}` }}
-                />
-              }
-              content={<Button color="grey" content="Log in to like this" onClick={() => { FlowRouter.go('/login'); }} />}
-              on="click"
-              position="top left"
+        <Popup
+          trigger={
+            <Button
+              color="red"
+              content="Like"
+              icon="heart"
+              label={{ basic: true, color: 'red', pointing: 'left', content: `${props.numLikes}` }}
             />
-          </div>
-        }
+            }
+          content={<Button color="grey" content="Log in to like this" onClick={() => { FlowRouter.go('/login'); }} />}
+          on="click"
+          position="top left"
+        />
       </div>
-    );
-  }
-}
+      }
+  </div>
+  );
 
 Likes.propTypes = {
   numLikes: PropTypes.number.isRequired,
   clickLike: PropTypes.func.isRequired,
 };
 
+export default Likes;
