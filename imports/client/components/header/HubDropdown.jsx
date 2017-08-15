17b626617e5d660718604aafb42ff421c1158c0f
Wei Hao <tanweihao94@gmail.com>
2017-08-15 18:38:50 +0800

    Added redirect to the dropdown menu items. Added a 'All' option

--- imports/client/components/header/HubDropdown.jsx
+++ imports/client/components/header/HubDropdown.jsx
@@ -1,34 +1,25 @@
 import React from 'react';
-import { Dropdown, Image} from 'semantic-ui-react';
+import { Dropdown, Image } from 'semantic-ui-react';
 import Hubs from '../../../api/hubs.js';
 
-let hubOptions = Hubs.map(hubObject => ({
-  key: hubObject.hubName,
-  text: hubObject.hubName,
-  value: hubObject.hubName,
-  image: { src: hubObject.image },
-  as: 'a',
-  href: `/hub/${hubObject.hubName}`,
-}));
-
-hubOptions = [{
-  key: 'All',
-  text: 'All',
-  value: 'All',
-  as: 'a',
-  href: '/' }, ...hubOptions];
+const allOption = (
+  <Dropdown.Item as="a" href={'/'}>
+    <Image src='/assets/img/earth-globe.png' inline size='mini' />
+    All
+  </Dropdown.Item>);
+const hubOptions = Hubs.map(hubObject => (
+  <Dropdown.Item key={hubObject.hubName} as="a" href={`/hub/${hubObject.hubName}`}>
+    <Image src={hubObject.image} inline size='mini' />
+    {hubObject.hubName}
+  </Dropdown.Item>));
 const dropdownStyle = {
   width: '200px',
-}
+};
 const HubDropdown = () => (
   <Dropdown item text="Hubs" icon="grid layout">
     <Dropdown.Menu style={dropdownStyle}>
-      {Hubs.map(hubObject => (
-        <Dropdown.Item key={hubObject.hubName} >
-          <Image src={hubObject.image} size='small'/>
-          {hubObject.hubName}
-        </Dropdown.Item>
-      ))}
+      {allOption}
+      {hubOptions}
     </Dropdown.Menu>
   </Dropdown>
 );
