8bff28010555ed5fcbc545d63ff161f55ef73706
FausticSun <FausticSun@gmail.com>
2017-08-23 17:05:21 +0800

    Added battlestations hub

--- imports/api/hubs.js
+++ imports/api/hubs.js
@@ -3,9 +3,23 @@
  */
 
 const Hubs = [
-  { hubName: 'EDC', image: '/assets/img/backpack.png', fullName: 'Everyday Carry: A peek into the pockets of people worldwide, showcasing our every day essentials.' },
-  { hubName: 'MFA', image: '/assets/img/mfa.png', fullName: 'Male Fashion Advice: Dressed by the Internet' },
-  { hubName: 'Fashion', image: '/assets/img/dress.png', fullName: 'Fashion: Dress your world' },
+  {
+    hubName: 'EDC',
+    image: '/assets/img/backpack.png',
+    fullName: 'Everyday Carry: A peek at our every day essentials',
+  }, {
+    hubName: 'MFA',
+    image: '/assets/img/mfa.png',
+    fullName: 'Male Fashion Advice: Dressed by the Internet',
+  }, {
+    hubName: 'Fashion',
+    image: '/assets/img/dress.png',
+    fullName: 'Fashion: Dress your world',
+  }, {
+    hubName: 'Battlestations',
+    image: '/assets/img/battlestations.png',
+    fullName: 'Battlestations: Desktop setups for maximum performance',
+  },
 ];
 
 export default Hubs;
